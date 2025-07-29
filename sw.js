82fc7d13e0f1a85c6acaded672535ba1d874f922
Intai <jhajan634@gmail.com>
2025-07-29 17:27:14 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// A minimal service worker to enable the PWA installation prompt.
+self.addEventListener('fetch', (event) => {
+  // This service worker is intentionally left blank.
+  // It's present to make the web app installable.
+});
