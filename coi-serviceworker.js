b2181e21201bdb781ebe066f88e27672b93815c1
Intai <jhajan634@gmail.com>
2025-12-10 09:33:42 +0900

    Create coi-serviceworker.js

---
+++ coi-serviceworker.js
@@ -0,0 +1,47 @@
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window === 'undefined') {
+  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+  self.addEventListener("fetch", function (event) {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+      return;
+    }
+
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (response.status === 0) {
+            return response;
+          }
+
+          const newHeaders = new Headers(response.headers);
+          newHeaders.set("Cross-Origin-Embedder-Policy",
+            coepCredentialless ? "credentialless" : "require-corp"
+          );
+          if (!newHeaders.get("Cross-Origin-Opener-Policy")) {
+            newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+          }
+
+          return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders,
+          });
+        })
+        .catch((e) => console.error(e))
+    );
+  });
+} else {
+  (async function () {
+    if (window.crossOriginIsolated !== false) return;
+
+    const registration = await navigator.serviceWorker.register(window.document.currentScript.src).catch((e) => console.error("COI: ", e));
+
+    if (registration) {
+      console.log("COI: Service Worker registered. Reloading page...");
+      window.location.reload();
+    }
+  })();
+}
